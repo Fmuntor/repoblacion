9d95fe1727860fcf7c2672c300e80e681f6ff051
Fmuntor <dam209f@gmail.com>
2023-01-18 18:06:17 +0100

    Especie

---
+++ src/modelo/Especie.java
@@ -0,0 +1,28 @@
+package modelo;
+
+public enum Especie {
+    ALAMO("álamo"),
+    ENCINA("encina"),
+    CASTANO("castaño"),
+    CIPRES("ciprés"),
+    PINO("pino"),
+    ROBLE("roble"),
+    OLIVO("olivo");
+
+    private final String especie;
+
+    public String cadenaAMostrar(String especie){
+        return this.especie;
+    }
+
+    Especie(String especie){
+        this.especie = especie;
+    }
+
+    @Override
+    public String toString() {
+        return especie;
+    }
+
+    
+}
